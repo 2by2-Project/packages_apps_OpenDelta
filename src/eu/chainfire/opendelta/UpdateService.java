681e009877f03bc04392b8d3f4f0e18c7dcda186
kailua <kailua@project2by2.jp>
2025-04-06 00:48:19 +0900

    UpdateService: Bypass filename parsing for now
    * Will fix later

--- src/eu/chainfire/opendelta/UpdateService.java
+++ src/eu/chainfire/opendelta/UpdateService.java
@@ -1468,13 +1468,13 @@ public class UpdateService extends Service implements OnSharedPreferenceChangeLi
                             }
                             Logger.d("parsed from json:");
                             Logger.d("fileName= " + fileName);
-                            if (!isMatchingImage(fileName)) {
+                            /*if (!isMatchingImage(fileName)) {
                                 String[] parts = fileName.split("-", 3);
                                 String ver = mConfig.getAndroidVersion();
                                 if (parts.length > 1) ver = parts[1];
                                 mState.update(State.ERROR_UNOFFICIAL, ver);
                                 return;
-                            }
+                            }*/
                             latestBuild = fileName;
                             if (urlOverride != null && !urlOverride.equals(""))
                                 Logger.d("url= " + urlOverride);
